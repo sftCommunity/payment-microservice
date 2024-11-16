f2c30d6e935bfec815dc8cf6b1ff4152e7ee97d0
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 02:00:51 -0500

    refactor: paymentService 're ensures that the payment session is properly created using the Stripe API.

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -5,4 +5,31 @@ import Stripe from 'stripe';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {},
+      },
+
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-shirt',
+            },
+            unit_amount: 2000, // 20.00
+          },
+          quantity: 2,
+        },
+      ],
+
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancel',
+    });
+
+    return session;
+  }
 }
