8d6ea2119f73dfbdbecc1c3ac782956e8a12cff4
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 03:23:50 -0500

    feat: add ValidationPipe to global pipes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 
 async function bootstrap() {
-  const logger = new Logger('Payment microservice');
+  const logger = new Logger('PaymentsMicroservice');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   app.setGlobalPrefix('api');
-
   await app.listen(envs.port);
   logger.log(`Payment microservice is running on port ${envs.port}`);
 }
