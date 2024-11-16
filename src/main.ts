4edc788e5e7ce0ae3907e748021eada79cdb5d87
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 01:24:40 -0500

    refactor: Import and use Logger and envs setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Payment microservice');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  logger.log(`Payment microservice is running on port ${envs.port}`);
 }
 bootstrap();
