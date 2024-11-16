00eb3da24230a2ecbe7344f44c96f76cb33efbaf
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 03:26:08 -0500

    refactor: update createPaymentSession method to recive paymentSessionDto

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import type { PaymentSessionDto } from './dto/payment-session.dto';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
@@ -6,8 +7,8 @@ export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Post('create-payment-session')
-  createPaymentSession() {
-    return this.paymentsService.createPaymentSession();
+  createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
+    return paymentSessionDto
   }
 
   @Get('success')
