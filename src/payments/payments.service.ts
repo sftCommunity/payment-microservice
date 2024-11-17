98a35ff661453e1ec73679e59e844ea80d4474c3
David Agámez Cantillo <davidagamez04@gmail.com>
2024-11-16 20:41:22 -0500

    refactor: update createPaymentSession method to use paymentSessionDto items

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -8,24 +8,24 @@ export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const lineItems = items.map((item) => ({
+      price_data: {
+        currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
     const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
         metadata: {},
       },
-
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000, // 20.00
-          },
-          quantity: 2,
-        },
-      ],
-
+      line_items: lineItems,
       mode: 'payment',
       success_url: 'http://localhost:3003/payments/success',
       cancel_url: 'http://localhost:3003/payments/cancel',
